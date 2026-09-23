50be2288bb0e33d8577efb5ab9da5a4cf4238ce9
amit-rgb <63780141+amit-rgb@users.noreply.github.com>
2026-09-23 10:16:24 +0530

    Add Spring Boot application entry point

---
+++ src/main/java/com/bheemnagartimes/news/NewsApplication.java
@@ -0,0 +1,4 @@
+package com.bheemnagartimes.news;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+@SpringBootApplication public class NewsApplication { public static void main(String[] args){ SpringApplication.run(NewsApplication.class,args); } }
